98ecdd7719425b49096b3c7bf9dcf2187a40cbd8
Ahmed Mossad <mossadahmed975@gmail.com>
2025-10-29 01:13:28 +0300

    add firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,13 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 import 'package:zytronic_whatsapp_task/app.dart';
 import 'package:zytronic_whatsapp_task/di/injection.dart';
+import 'package:zytronic_whatsapp_task/firebase_options.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   configureDependencies();
   await Hive.initFlutter();
   runApp(const MyApp());
